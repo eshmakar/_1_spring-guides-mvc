b899ce66a48d7b1cea511dc417a4c1d9bac45e1e
Marat <eshmakar@mail.ru>
2020-08-19 23:10:18 +0300

    Initial commit

---
+++ src/test/java/ru/eshmakar/_1_springguidesmvc/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eshmakar._1_springguidesmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
