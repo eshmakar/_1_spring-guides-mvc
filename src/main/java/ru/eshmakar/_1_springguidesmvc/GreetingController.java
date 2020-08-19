85d2d02a8c9c14ede421a72d171e9a9fd1cf2a62
Marat <eshmakar@mail.ru>
2020-08-20 00:03:38 +0300

    thymeleaf was to change to mustache and was added main.mustache to view index main

--- src/main/java/ru/eshmakar/_1_springguidesmvc/GreetingController.java
+++ src/main/java/ru/eshmakar/_1_springguidesmvc/GreetingController.java
@@ -5,12 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 
+
+    //this Controller work with mustache
     @GetMapping("greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model){
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name,
+                           Map<String, Object> model){
+
+        model.put("name", name);
         return "greeting";
+    }
+
+
+    @GetMapping
+    public String main(Map<String, Object> model){ //здесь уже необходимо сделать список, без этого не работает
+        model.put("some", "hello, Eshmakar!"); //put - это синоним add.atribute (thymeleaf)
+        return "main";
     }
 }
